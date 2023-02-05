ec6fbc69f9a6d65e29d77b18f0f6fc24b7c919e1
Matheus de Paulo Oliveira <matheusoliveira1991@hotmail.com>
2023-02-05 16:19:27 -0300

    log added

--- src/main/java/br/com/dolomia/sysxl/database/ConnectionDolomiaDB.java
+++ src/main/java/br/com/dolomia/sysxl/database/ConnectionDolomiaDB.java
@@ -1,25 +1,36 @@
 package br.com.dolomia.sysxl.database;
 
 import br.com.dolomia.sysxl.utils.GetProperties;
+import br.com.dolomia.sysxl.utils.AlertScreen;
+import br.com.dolomia.sysxl.utils.LogUtils;
 import br.com.dolomia.sysxl.utils.SelectDB;
+import javafx.scene.control.Alert;
 
 import java.sql.*;
-import java.util.Properties;
 
 public class ConnectionDolomiaDB {
 
     private static Connection getConnection() {
         try {
+            LogUtils.registerLog("Url: " + GetProperties.getProperty("url"));
+            LogUtils.registerLog("User: " + GetProperties.getProperty("user"));
+            LogUtils.registerLog("Password: " + GetProperties.getProperty("passwd"));
             return DriverManager.getConnection(GetProperties.getProperty("url"), GetProperties.getProperty("user"), GetProperties.getProperty("passwd"));
         } catch (SQLException e) {
+            AlertScreen.getAlert("ERRO", "ERRO AO SE CONECTAR COM O BANCO DE DADOS", e.getMessage(), Alert.AlertType.ERROR);
+            LogUtils.registerError("Error connecting to DB -> " + e.getMessage());
             throw new RuntimeException(e);
         }
     }
 
     private static PreparedStatement getPreparedStatement(Integer monthSelected, Integer yearSelected) {
+        LogUtils.registerLog("====================[DOLOMIADB -> CONNECTED!]====================");
+
         try {
             return getConnection().prepareStatement(SelectDB.getSelectDB(monthSelected, yearSelected));
         } catch (SQLException e) {
+            AlertScreen.getAlert("ERRO", "ERRO AO EXECUTAR QUERY (SELECT) NO BANCO DE DADOS", e.getMessage(), Alert.AlertType.ERROR);
+            LogUtils.registerError("Error executing query -> " + e.getMessage());
             throw new RuntimeException(e);
         }
     }
@@ -28,6 +39,8 @@ public class ConnectionDolomiaDB {
         try {
             return getPreparedStatement(monthSelected, yearSelected).executeQuery();
         } catch (SQLException e) {
+            AlertScreen.getAlert("ERRO", "ERRO AO EXECUTAR QUERY (SELECT) NO BANCO DE DADOS", e.getMessage(), Alert.AlertType.ERROR);
+            LogUtils.registerError("Error executing query -> " + e.getMessage());
             throw new RuntimeException(e);
         }
     }
