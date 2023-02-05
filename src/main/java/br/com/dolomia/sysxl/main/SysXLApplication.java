15da80b07224c7d6ae4bb3f35bbbdb12c37ab8c2
Matheus de Paulo Oliveira <matheusoliveira1991@hotmail.com>
2023-02-05 03:26:34 -0300

    application main class

---
+++ src/main/java/br/com/dolomia/sysxl/main/SysXLApplication.java
@@ -0,0 +1,31 @@
+package br.com.dolomia.sysxl.main;
+
+import br.com.dolomia.sysxl.utils.GetProperties;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SysXLApplication extends Application {
+    @Override
+    public void start(Stage stage) {
+        try {
+            AnchorPane root = FXMLLoader.load(getClass().getResource("Layout.fxml"));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("Layout.css").toExternalForm());
+            stage.setScene(scene);
+            stage.setTitle(GetProperties.getProperty("version"));
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
