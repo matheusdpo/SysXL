d5e1b644f7e837f9f5cb9230c30a4472a70ab9dd
Matheus de Paulo Oliveira <matheusoliveira1991@hotmail.com>
2023-02-05 03:24:52 -0300

    test class added

---
+++ src/main/java/br/com/dolomia/sysxl/test/TestApp.java
@@ -0,0 +1,14 @@
+package br.com.dolomia.sysxl.test;
+
+import br.com.dolomia.sysxl.utils.GetProperties;
+
+public class TestApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(GetProperties.getProperty("user"));
+
+    }
+
+
+}
