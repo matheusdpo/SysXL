3f40f1ddee905c04e17545a1847ae67df5c5f955
Matheus de Paulo Oliveira <matheusoliveira1991@hotmail.com>
2023-02-05 03:21:22 -0300

    file handler added

---
+++ src/main/java/br/com/dolomia/sysxl/excel/ExcelFileHandler.java
@@ -0,0 +1,52 @@
+package br.com.dolomia.sysxl.excel;
+
+import br.com.dolomia.sysxl.bean.ExcelBean;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+
+public class ExcelFileHandler {
+
+    public static void init(ExcelBean excelBean, String path) {
+        XSSFWorkbook workbook = null;
+        try {
+            workbook = new XSSFWorkbook();
+            XSSFSheet sheet = workbook.createSheet("Sysfogo");
+
+            for (int i = 0; i < excelBean.getCnpj_fornecedor().size(); i++) {
+                XSSFRow row = sheet.createRow(i);
+
+                if (i == 0) {
+                    row.createCell(0).setCellValue("cnpj_fornecedor");
+                    row.createCell(1).setCellValue("num_nf");
+                    row.createCell(2).setCellValue("guia_trafego");
+                    row.createCell(3).setCellValue("cod_produto");
+                    row.createCell(4).setCellValue("den_item");
+                    row.createCell(5).setCellValue("iis_embal");
+                    row.createCell(6).setCellValue("dat_exportacao");
+                    continue;
+                }
+                row.createCell(0).setCellValue(excelBean.getCnpj_fornecedor().get(i - 1));
+                row.createCell(1).setCellValue(excelBean.getNum_nf().get(i - 1));
+                row.createCell(2).setCellValue(excelBean.getGuia_trafego().get(i - 1));
+                row.createCell(3).setCellValue(excelBean.getCod_produto().get(i - 1));
+                row.createCell(4).setCellValue(excelBean.getDen_item().get(i - 1));
+                row.createCell(5).setCellValue(excelBean.getIis_embal().get(i - 1));
+                row.createCell(6).setCellValue(excelBean.getDat_exportacao().get(i - 1));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                FileOutputStream fileOut = new FileOutputStream(path);
+                workbook.write(fileOut);
+                fileOut.close();
+                workbook.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
