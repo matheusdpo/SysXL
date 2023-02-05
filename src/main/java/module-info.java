9d622194f72740d193200f12c3086d9f71d8ca3d
Matheus de Paulo Oliveira <matheusoliveira1991@hotmail.com>
2023-02-05 16:21:19 -0300

    log and junit added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,8 @@ module br.com.dolomia.sysxl {
     requires lombok;
     requires java.sql;
     requires poi.ooxml;
+    requires org.apache.logging.log4j;
+    requires org.junit.jupiter.api;
 
 
     opens br.com.dolomia.sysxl.main to javafx.fxml;
